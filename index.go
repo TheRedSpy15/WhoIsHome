cabfdbd721de38ed7661fd0f93aafd3fbaf26ad0
TheRedSpy15 <hjadar15@protonmail.com>
2020-04-24 04:51:53 -0400

    refactoring, basic auth, body limit, error handling, gzip

--- index.go
+++ index.go
@@ -2,41 +2,36 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"net/http"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/labstack/echo/v4"
 )
 
-type Car struct {
-	ID      int    `json:"id"`
-	Name    string `json:"name"`
-	Present bool   `json:"present"`
-}
-
 func index(c echo.Context) error {
+	// Database connection
 	db, err := sql.Open("mysql", "hunter:HDrumm_17@tcp(127.0.0.1:3306)/testdatabase") // TODO: read csv
 	if err != nil {
-		panic(err.Error())
+		panic("MySQL: Failed to open database")
 	} else {
-		c.Logger().Print("MySQL: Connection Established")
+		fmt.Println("MySQL: Connection Established")
 	}
-
 	err = db.Ping()
 	if err != nil {
-		panic(err.Error()) // need proper error handling instead of panic
+		fmt.Println("MySQL: Database ping failed: \n" + err.Error())
 	}
-	defer db.Close()
 
 	results, err := db.Query("SELECT * FROM cars")
 	defer results.Close()
 
+	// Displaying data
 	var viewData string
 	for results.Next() {
 		var car Car
 		err = results.Scan(&car.ID, &car.Name, &car.Present)
 		if err != nil {
-			panic(err.Error()) // proper error handling instead of panic in your app
+			fmt.Println("Failed to read data")
 		}
 		viewData += `<p style="width:20px;height:20px;">` + car.Name + `</p>` // TODO: read from template file
 	}
--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"crypto/subtle"
+	"database/sql"
+
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
+
+var db *sql.DB
 
 func main() {
 	e := echo.New()
@@ -12,6 +17,17 @@ func main() {
 	// Middleware
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
+	e.Use(middleware.GzipWithConfig(middleware.GzipConfig{
+		Level: 5,
+	}))
+	e.Use(middleware.BasicAuth(func(username, password string, c echo.Context) (bool, error) {
+		if subtle.ConstantTimeCompare([]byte(username), []byte("joe")) == 1 && // TODO: need to read from csv
+			subtle.ConstantTimeCompare([]byte(password), []byte("secret")) == 1 {
+			return true, nil
+		}
+		return false, nil
+	}))
+	e.Use(middleware.BodyLimit("2M"))
 
 	// Routes
 	e.GET("/", index)
