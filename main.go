0231b5a5057f94e7cdf202bd2ffd30d42a9df517
TheRedSpy15 <hjadar15@protonmail.com>
2020-04-25 03:27:41 -0400

    auth now reads from csv

--- main.go
+++ main.go
@@ -4,15 +4,18 @@ import (
 	"crypto/subtle"
 	"database/sql"
 
+	"github.com/TheRedSpy15/WhoIsHome/utils"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
 var db *sql.DB
+var csv [4]string
 
 func main() {
 	e := echo.New()
+	csv = utils.ReadCsvFile("OpenCV/database.csv")
 
 	// Middleware
 	e.Use(middleware.Logger())
@@ -21,8 +24,8 @@ func main() {
 		Level: 5,
 	}))
 	e.Use(middleware.BasicAuth(func(username, password string, c echo.Context) (bool, error) {
-		if subtle.ConstantTimeCompare([]byte(username), []byte("joe")) == 1 && // TODO: need to read from csv
-			subtle.ConstantTimeCompare([]byte(password), []byte("secret")) == 1 {
+		if subtle.ConstantTimeCompare([]byte(username), []byte(csv[2])) == 1 &&
+			subtle.ConstantTimeCompare([]byte(password), []byte(csv[3])) == 1 {
 			return true, nil
 		}
 		return false, nil
